7d0ca98f883da4bbeef5fdaf9ea0cbe93fabb1a0
Maya-Bitter <mayabitter@gmail.com>
2023-04-19 14:43:57 +0200

    updated the code

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,26 @@
+// Here you import the PropTypes library
+import PropTypes from "prop-types";
+
+// The MovieCard function component 
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
+  return (
+    <div
+      onClick={() => {
+onMovieClick(movie);      }}
+    >
         {movie.title} 
-      </div>
-    );
-  };
+    </div>
+  );
+};
+
+// Here is where we define all the props constraints for the MovieCard
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
 
  
