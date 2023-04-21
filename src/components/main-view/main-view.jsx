b0cb9244a4db4637f59bb2484bbb62a1a4ff017b
Maya-Bitter <mayabitter@gmail.com>
2023-04-21 11:59:34 +0200

    .

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,31 +5,56 @@ import { LoginView } from "../login-view/login-view.jsx";
 import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
-const [movies, setMovies] = useState([]);
-const [selectedMovie, setSelectedMovie] = useState(null);
-const [user, setUser] = useState(null);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
 useEffect(() => {
-fetch("https://m-flix.herokuapp.com/movies")
+if (!token) return;
+
+fetch("https://m-flix.herokuapp.com/movies", {
+headers: { Authorization: `Bearer ${token}` },
+})
 .then((response) => response.json())
 .then((movies) => {
-  const moviesFromApi = movies.map((movie) => {
-    return {
-      id: movie._id,
-      title: movie.Title, 
-      genre: movie.Genre.Name,
-      description: movie.Description,
-      image: movie.Image,
-      director: movie.Director.Name?.[0],
-      };
-  });
 
-  setMovies(moviesFromApi);
+const moviesFromApi = movies.map((movie) => {
+  return {
+    id: movie._id,
+    title: movie.Title, 
+    genre: movie.Genre.Name,
+    description: movie.Description,
+    image: movie.Image,
+    director: movie.Director.Name?.[0],
+    };
 });
-}, []);
+  setMovies(movies);
+
+});
+}, [token]);
+
+ // rest of the code
 
 if (!user) {
-  return <LoginView onLoggedIn={(user) => setUser(user)} />;
+return (
+<LoginView
+  onLoggedIn={(user, token) => {
+    setUser(user);
+    setToken(token);
+  }}
+/>
+);
+}
+
+if (data.user) {
+  localStorage.setItem("user", JSON.stringify(data.user));
+  localStorage.setItem("token", data.token);
+  onLoggedIn(data.user, data.token);
+} else {
+  alert("No such user");
 }
 
 if (selectedMovie) {
