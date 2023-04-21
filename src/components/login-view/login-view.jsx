00431178c7db86a1f3c54aac3ae513aed2943b51
Maya-Bitter <mayabitter@gmail.com>
2023-04-21 10:57:19 +0200

    login view update

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,51 @@
-export const LoginView = () => {
-    return (
-      <form>
-        <label>
-          Username:
-          <input type="text" />
-        </label>
-        <label>
-          Password:
-          <input type="password" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    );
-  };
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+onst [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+const handleSubmit = (event) => {
+// this prevents the default behavior of the form which is to reload the entire page
+event.preventDefault();
+
+const data = {
+  access: username,
+  secret: password
+};
+
+fetch("https://m-flix.herokuapp.com/users", {
+method: "POST",
+body: JSON.stringify(data)
+}).then((response) => {
+if (response.ok) {
+  onLoggedIn(username);
+} else {
+  alert("Login failed");
+}
+});
+};
+return (
+<form onSubmit={handleSubmit}>
+<label>
+  Username:
+  <input
+    type="text"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+    required
+    minLength="3"
+  />
+</label>
+<label>
+  Password:
+  <input
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    required
+  />
+</label>
+<button type="submit">Submit</button>
+</form>
+);
+};
