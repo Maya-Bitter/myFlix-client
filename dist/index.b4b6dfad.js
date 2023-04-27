fa8b4cea972d1a70f256a6b4cb0f954cd264e91f
Maya-Bitter <mayabitter@gmail.com>
2023-04-27 12:11:09 +0200

    updated the app name in the navbar

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -46690,7 +46690,7 @@ const NavigationBar = ({ user , onLoggedOut  })=>{
                 /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Navbar).Brand, {
                     as: (0, _reactRouterDom.Link),
                     to: "/",
-                    children: "Books App"
+                    children: "MyFlix App"
                 }, void 0, false, {
                     fileName: "src/components/navigation-bar/navigation-bar.jsx",
                     lineNumber: 8,
