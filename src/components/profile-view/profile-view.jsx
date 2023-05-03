7b1cb916da662a04eb2c859ec2c70c02f5e28c5a
Maya-Bitter <mayabitter@gmail.com>
2023-05-03 15:37:07 +0200

    updated the user display code

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,32 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
-const ProfileView = () => {
-   const [users, setUsers] = useState([]);
-   useEffect(() => {
-      fetch('https://m-flix.herokuapp.com/users')
-         .then((response) => response.json())
-         .then((data) => {
-            console.log(data);
-            setUsers(data);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-   }, []);
+export const ProfileView = ({ user }) => {
 
-   return (
-    <div className="user-container">
-       {users.map((user) => {
-          return (
-             <div className="post-card" key={user.Username}>
-                <h2 className="post-title">{user.password}</h2>
-                <p className="post-body">{user.email}</p>
-                <p className="post-body">{user.birthday}</p>
-            </div>
-          );
-       })}
-    </div>
-    );
- };
- 
- export default ProfileView; 
+return (
+   <div className="user-container">
+      <div classname="user-card">
+      <p className="Username">{user.Username}</p>
+      <p className="Password">{user.Email}</p>
+</div>
+</div>
+);
+};
+
+
+
+
