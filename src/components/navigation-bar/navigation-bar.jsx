804148b00b3224299d7bab355b2c7d6f53c46ca0
Maya-Bitter <mayabitter@gmail.com>
2023-05-11 13:38:10 +0200

    navbar color change

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
-    <Navbar bg="primary" expand="lg">
+    <Navbar bg="primary" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
           MyFlix
